9052f83ec5bddd61ebf8354961103c81fbb7a034
yashtyagi298 <tyagiyash097@gmail.com>
2024-11-26 15:07:25 +0530

    BE done without Socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,19 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import cookieparser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routers/auth.route.js";
+import messageRoute from "./routers/message.route.js"
 dotenv.config()
 
 const app= express();
+app.use(express.json());
+app.use(cookieparser());
 const PORT = process.env.PORT
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoute);
 app.listen(PORT, () => {
     console.log(` http://localhost:${PORT}`);
     connectDB();
